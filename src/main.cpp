8b95ca365533224f5a6bb3492b3592c703506977
Spectre <antoine@doussaud.org>
2022-05-27 23:57:02 +0200

    add pront DMX adress at start

--- src/main.cpp
+++ src/main.cpp
@@ -564,7 +564,20 @@ void DMX_task(void* parameter) {
 	delay(10);
 
 	DMX::Initialize(input);
-	Serial.println("initialized...");
+	Serial.println("DMX initialized...");
+
+	uint16_t dip = (digitalRead(DIP_PIN_0) ? 0 : 1)
+		| (digitalRead(DIP_PIN_1) ? 0 : 1 << 1)
+		| (digitalRead(DIP_PIN_2) ? 0 : 1 << 2)
+		| (digitalRead(DIP_PIN_3) ? 0 : 1 << 3)
+		| (digitalRead(DIP_PIN_4) ? 0 : 1 << 4)
+		| (digitalRead(DIP_PIN_5) ? 0 : 1 << 5)
+		| (digitalRead(DIP_PIN_6) ? 0 : 1 << 6)
+		| (digitalRead(DIP_PIN_7) ? 0 : 1 << 7)
+		| (digitalRead(DIP_PIN_8) ? 0 : 1 << 8)
+		| (digitalRead(DIP_PIN_9) ? 0 : 1 << 9);
+	Serial.printf("Adress DMX: %d\n", dip);
+
 	wipe();
 	uint8_t ctn = 0;
 	uint8_t test_mode = 0;
